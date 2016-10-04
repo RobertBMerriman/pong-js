247f15ab4d3c0cce3ef8d56ab0e10678c160e265
Robert Merriman <robertbmerriman@gmail.com>
2016-10-04 15:20:16 +0100

    Fill background

---
+++ js/pong.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+
+const canvas = document.getElementById("pong");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#000";
+
+context.fillRect(0, 0, canvas.width, canvas.height);
